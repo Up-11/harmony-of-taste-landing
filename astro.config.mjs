24d423933528ac4afafe4959d603553eb421c424
up <iurykolgin@gmail.com>
2025-06-18 02:25:17 +0700

    add allowedHost

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
-import vue from '@astrojs/vue';
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
+		preview: {
+			allowedHosts: [
+				'harmoney-of-taste-landing-wevnxj-900d6f-5-129-207-103.traefik.me',
+			],
+		},
 	},
 
-  integrations: [vue()],
+	integrations: [vue()],
 })
